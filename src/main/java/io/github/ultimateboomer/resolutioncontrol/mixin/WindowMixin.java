36453b24ec3f05b263a04ef31287985143d9bb8c
freymond <37636251+freymend@users.noreply.github.com>
2024-02-25 21:53:41 -0800

    remove unused mixin injection - add unique to scale() get rid of warning - getScaleFactor() injection never seemed to do anything - add documentation mixin injections - onUpdateFrameBufferSize() was never called

--- src/main/java/io/github/ultimateboomer/resolutioncontrol/mixin/WindowMixin.java
+++ src/main/java/io/github/ultimateboomer/resolutioncontrol/mixin/WindowMixin.java
@@ -4,6 +4,7 @@ import io.github.ultimateboomer.resolutioncontrol.ResolutionControlMod;
 import net.minecraft.client.util.Window;
 import net.minecraft.util.math.MathHelper;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -11,34 +12,38 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Window.class)
 public abstract class WindowMixin {
+  /**
+   * This method modifies the width of the FrameBuffer to match the current scale factor of the mod, which causes
+   * the game to render at the resolution of the scaled window. But this only resizes the Client Window FrameBuffer,
+   * not the other FrameBuffers used in rendering the game.
+   */
   @Inject(at = @At("RETURN"), method = "getFramebufferWidth", cancellable = true)
   private void getFramebufferWidth(CallbackInfoReturnable<Integer> ci) {
     ci.setReturnValue(scale(ci.getReturnValueI()));
   }
 
+  /**
+   * This method modifies the width of the FrameBuffer to match the current scale factor of the mod, which causes
+   * the game to render at the resolution of the scaled window. But this only resizes the Client Window FrameBuffer,
+   * not the other FrameBuffers used in rendering the game.
+   */
   @Inject(at = @At("RETURN"), method = "getFramebufferHeight", cancellable = true)
   private void getFramebufferHeight(CallbackInfoReturnable<Integer> ci) {
     ci.setReturnValue(scale(ci.getReturnValueI()));
   }
 
+  @Unique
   private int scale(int value) {
     double scaleFactor = ResolutionControlMod.getInstance().getCurrentScaleFactor();
     return Math.max(MathHelper.ceil((double) value * scaleFactor), 1);
   }
 
-  @Inject(at = @At("RETURN"), method = "getScaleFactor", cancellable = true)
-  private void getScaleFactor(CallbackInfoReturnable<Double> ci) {
-    ci.setReturnValue(
-        ci.getReturnValueD() * ResolutionControlMod.getInstance().getCurrentScaleFactor());
-  }
-
+  /**
+   * This method is called when the client window is resized. We need to resize the FrameBuffers to
+   * match the new window size.
+   */
   @Inject(at = @At("RETURN"), method = "onFramebufferSizeChanged")
   private void onFramebufferSizeChanged(CallbackInfo ci) {
-    ResolutionControlMod.getInstance().onResolutionChanged();
-  }
-
-  @Inject(at = @At("RETURN"), method = "updateFramebufferSize")
-  private void onUpdateFramebufferSize(CallbackInfo ci) {
     ResolutionControlMod.getInstance().onResolutionChanged();
   }
 }
