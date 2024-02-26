12dc1c849575ec0ca6215dcf0e04c5da33ef477b
freymond <37636251+freymend@users.noreply.github.com>
2024-02-26 11:51:35 -0800

    update documentation in WindowMixin.java

--- src/main/java/io/github/ultimateboomer/resolutioncontrol/mixin/WindowMixin.java
+++ src/main/java/io/github/ultimateboomer/resolutioncontrol/mixin/WindowMixin.java
@@ -13,9 +13,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(Window.class)
 public abstract class WindowMixin {
   /**
-   * This method modifies the width of the FrameBuffer to match the current scale factor of the mod, which causes
-   * the game to render at the resolution of the scaled window. But this only resizes the Client Window FrameBuffer,
-   * not the other FrameBuffers used in rendering the game.
+   * This method modifies the width of the FrameBuffer to match the current scale factor of the mod,
+   * which causes the game to render at the resolution of the scaled window. But this only resizes
+   * the Client Window FrameBuffer, not the other FrameBuffers used in rendering the game. The
+   * rendering of the GUI isn't affected.
    */
   @Inject(at = @At("RETURN"), method = "getFramebufferWidth", cancellable = true)
   private void getFramebufferWidth(CallbackInfoReturnable<Integer> ci) {
@@ -23,24 +24,31 @@ public abstract class WindowMixin {
   }
 
   /**
-   * This method modifies the width of the FrameBuffer to match the current scale factor of the mod, which causes
-   * the game to render at the resolution of the scaled window. But this only resizes the Client Window FrameBuffer,
-   * not the other FrameBuffers used in rendering the game.
+   * This method modifies the width of the FrameBuffer to match the current scale factor of the mod,
+   * which causes the game to render at the resolution of the scaled window. But this only resizes
+   * the Client Window FrameBuffer, not the other FrameBuffers used in rendering the game. The
+   * rendering of the GUI isn't affected.
    */
   @Inject(at = @At("RETURN"), method = "getFramebufferHeight", cancellable = true)
   private void getFramebufferHeight(CallbackInfoReturnable<Integer> ci) {
     ci.setReturnValue(scale(ci.getReturnValueI()));
   }
 
+  /**
+   * This method scales the value by the current scale factor of the mod. So if the scale factor is
+   * 2 and the game is rendered at 1920x1080, then the resolution will be doubled to 3840x2560. The
+   * value is rounded up to the nearest integer, and the minimum value is 1.
+   */
   @Unique
   private int scale(int value) {
     double scaleFactor = ResolutionControlMod.getInstance().getCurrentScaleFactor();
-    return Math.max(MathHelper.ceil((double) value * scaleFactor), 1);
+    int roundedScaleValue = MathHelper.ceil((double) value * scaleFactor);
+    return Math.max(roundedScaleValue, 1);
   }
 
   /**
-   * This method is called when the client window is resized. We need to resize the FrameBuffers to
-   * match the new window size.
+   * This method is called when the client window is resized. The game is rendered at the resolution
+   * of the scaled window, so we need to update the FrameBuffers to match the new window size.
    */
   @Inject(at = @At("RETURN"), method = "onFramebufferSizeChanged")
   private void onFramebufferSizeChanged(CallbackInfo ci) {
