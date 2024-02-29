dd7292c6f4de600a1fef64a956303aba126bc2ed
freymond <37636251+freymend@users.noreply.github.com>
2024-02-28 19:58:18 -0800

    remove unused import

--- src/main/java/io/github/ultimateboomer/resolutioncontrol/ResolutionControlMod.java
+++ src/main/java/io/github/ultimateboomer/resolutioncontrol/ResolutionControlMod.java
@@ -3,7 +3,6 @@ package io.github.ultimateboomer.resolutioncontrol;
 import io.github.ultimateboomer.resolutioncontrol.client.gui.screen.MainSettingsScreen;
 import io.github.ultimateboomer.resolutioncontrol.client.gui.screen.SettingsScreen;
 import io.github.ultimateboomer.resolutioncontrol.util.*;
-import java.util.Set;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
